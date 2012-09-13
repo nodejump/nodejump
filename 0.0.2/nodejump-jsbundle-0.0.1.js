1f58c44a8e9f2463eb0638dcc1022f80a480fa95
commit <commit@noemail.com>
2012-09-14 11:24:54 +1200

    working on improving startup speed

---
+++ 0.0.2/nodejump-jsbundle-0.0.1.js
@@ -0,0 +1,35 @@
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/nodejump-jsbundle-0.0.1 -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-core-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/js-toolbox-0.0.1") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-common-0.0.1") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-navbar-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-auth-0.0.1") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/aj-share-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/bootstrap-utils-js-0.0.1") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-share-ui-js-0.0.1") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-status-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-notification-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-progress-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/aj-edit-js-0.0.6") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/aj-view-0.0.4") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/new-link-dialog-js-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/nodejump-app-0.0.2") -->
+
+<!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/nodejump-helpbar-js-0.0.2") -->
+
+// <!-- one.end -->
