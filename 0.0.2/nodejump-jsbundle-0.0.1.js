5b722be5329d6f16b61c60c9290e484d1c9fb32e
commit <commit@noemail.com>
2012-09-14 11:27:35 +1200

    fixing dependecy issues

--- 0.0.2/nodejump-jsbundle-0.0.1.js
+++ 0.0.2/nodejump-jsbundle-0.0.1.js
@@ -32,4 +32,94 @@
 
 <!-- one.embedText("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/nodejump-helpbar-js-0.0.2") -->
 
+function AJ_loginCompleted(aj, userLoggedIn) {
+
+	
+
+	var client = aj.createClient();
+	
+	var lastInjectedHashValue = null;
+	
+	var helpBar = $.initHelpBar($("#helpbar"));
+	
+	var nodejump = $.initNodeJumpApp({
+		elem: $("#nodejumpapp"),
+		client: client,
+		nodeChangeHandler: function(nj, node, secret) {
+			if (!nj.isAnonymousUser()) {
+				window.location.hash = "#"+node.url();
+			} else {
+				window.location.hash = "#"+node.url()+"&"+secret;
+			}
+			lastInjectedHashValue = window.location.hash;
+		}
+	});
+	
+	nodejump.initComponents();
+	
+	nodejump.readHash(window.location.hash, function(hadHash) {
+		if (!hadHash) {
+			
+			if (AJ.userNodeUri) {
+					nodejump.initForUser(function() {
+						nodejump.startAutoCommit();
+						nodejump.startAutoRefresh();
+					} );
+			} else {
+				nodejump.initForAnonymous(function() {
+					nodejump.startAutoCommit();
+					nodejump.startAutoRefresh();
+				});
+			}
+			return;
+		}
+		
+		nodejump.startAutoCommit();
+		nodejump.startAutoRefresh();
+	});
+	
+		
+	
+	$(window).hashchange( function(){
+		if (!lastInjectedHashValue || !(window.location.hash.valueOf() === lastInjectedHashValue.valueOf())) {
+			
+			nodejump.readHash(window.location.hash, function(hadHash) {
+				
+			});
+		}
+		lastInjectedHashValue = null;
+	});
+	
+	 
+	$(window).focus(function() {
+		nodejump.startAutoCommit();
+		nodejump.startAutoRefresh();
+	});
+	
+	$(window).blur(function() {
+		nodejump.stopAutoCommit();
+		nodejump.stopAutoRefresh();
+	});
+
+	$(window).unload(function() {
+		nodejump.stopAutoCommit();
+		nodejump.stopAutoRefresh();
+	});
+	
+}
+
+function AJ_engineLoaded(aj) {
+
+	AJ.statusBar = $.ajInitStatusBar($("#ajstatusbar"), aj);
+	AJ.notificationBar = $.ajInitNotificationBar($("#ajnotificationbar"), aj);
+	AJ.navbar = $.ajInitNavbar($("#ajnavbar"), aj);
+	AJ.progressBar=$.ajInitProgressBar($("#ajprogressbar"));
+	
+	
+}
+
+window.onload = function() {
+
+}
+
 // <!-- one.end -->
